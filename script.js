c1bb77f0fea8fc7b30dd09b5fd90e4cfc3438860
Artonstyle <artonstyle@hotmail.fr>
2025-09-27 01:14:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 const socket = io();
 let pc = new RTCPeerConnection();
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
 
-const localVideo = document.getElementById("localVideo");
-const remoteVideo = document.getElementById("remoteVideo");
-
-navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-  .then(stream => {
-    localVideo.srcObject = stream;
-    stream.getTracks().forEach(track => pc.addTrack(track, stream));
-  });
-
-pc.ontrack = (event) => {
-  remoteVideo.srcObject = event.streams[0];
-};
-
-socket.on("match", async (partnerId) => {
-  const offer = await pc.createOffer();
-  await pc.setLocalDescription(offer);
-  socket.emit("signal", { to: partnerId, data: offer });
+navigator.mediaDevices.getUserMedia({video:true,audio:true})
+.then(stream => {
+  localVideo.srcObject = stream;
+  stream.getTracks().forEach(track => pc.addTrack(track, stream));
 });
 
-socket.on("signal", async ({ from, data }) => {
-  if (data.type === "offer") {
+pc.ontrack = (event) => { remoteVideo.srcObject = event.streams[0]; };
+
+socket.on('match', async (partnerId) => {
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
+  socket.emit('signal', { to: partnerId, data: offer });
+});
+
+socket.on('signal', async ({ from, data }) => {
+  if(data.type === 'offer'){
     await pc.setRemoteDescription(new RTCSessionDescription(data));
     const answer = await pc.createAnswer();
     await pc.setLocalDescription(answer);
-    socket.emit("signal", { to: from, data: answer });
-  } else if (data.type === "answer") {
+    socket.emit('signal', { to: from, data: answer });
+  } else if(data.type === 'answer'){
     await pc.setRemoteDescription(new RTCSessionDescription(data));
-  } else if (data.candidate) {
+  } else if(data.candidate){
     await pc.addIceCandidate(new RTCIceCandidate(data));
   }
 });
 
-pc.onicecandidate = (event) => {
-  if (event.candidate) {
-    socket.emit("signal", { data: event.candidate });
+pc.onicecandidate = (event)=>{
+  if(event.candidate){
+    socket.emit('signal',{data:event.candidate});
   }
 };
